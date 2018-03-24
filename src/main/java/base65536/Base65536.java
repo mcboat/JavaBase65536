709ff518b75c80902b045fdd1fd96150621f93be
coltongit <colton51919@gmail.com>
2018-03-24 12:32:43 -0300

    Added the groundwork for the main class. Encoder and decoder classes are soon to follow.

--- src/main/java/base65536/Base65536.java
+++ src/main/java/base65536/Base65536.java
@@ -1,7 +1,31 @@
 package base65536;
-
-public class Base65536 {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+import java.io.FilterOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+public class Base65536
+{
+    private Base65536() {}
+    /**
+     * Returns a {@link Encoder} that encodes using the
+     * <a href="#basic">Basic</a> type base65536 encoding scheme.
+     *
+     * @return  A Base65536 encoder.
+     */
+    public static Encoder getEncoder() {
+        return Encoder.Base65536;
+    }
+    {
+    /**
+     * Returns a {@link Decoder} that decodes using the
+     * <a href="#basic">Basic</a> type base65536 encoding scheme.
+     *
+     * @return  A Base65536 decoder.
+     */
+    public static Decoder getDecoder() {
+         return Decoder.Base65536;
+    }
+    }
 }
